17754fdaa6d901a5a2ca155da19d3045b6faeca6
Val Neekman <val@neekware.com>
2017-03-08 10:44:51 -0500

    all recursive backup of git directories

--- osx/git/gitBackup.sh
+++ osx/git/gitBackup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-declare -a gitDirectories=(`ls -d */ | cut -f1 -d'/'`)
+# Loops over all git directories and backs them up
 
-#loop over the array of directories and back them up
+declare -a gitDirectories=(`find . -type d -name .git | sed 's/.git//g'`)
+
 for i in ${gitDirectories[@]}; do
     echo "Processing $i ..."
     cd $i
     git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
     git fetch --all
     git pull --all
-    cd ..
+    cd -
 done
+
