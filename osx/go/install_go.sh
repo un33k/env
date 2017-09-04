1c6e2764867aaa73bbddd0414b8df2ccedbf4fff
Val Neekman <val@neekware.com>
2017-09-04 12:21:41 -0400

    add go support

---
+++ osx/go/install_go.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Ensure the following lines are sourced (.bash_profile, or .bash_go)
+# ===============================
+# export GOPATH=$HOME/Code/go
+# export PATH=$PATH:$GOPATH/bin
+
+brew update && brew upgrade
+brew install go
+brew install git
+brew install mercurial
+
+mkdir -p $GOPATH/src/github.com/user
+
+echo "installation done."
+echo "export GOPATH=${GOPATH}"
+
+go get code.google.com/p/go-tour/gotour
+gotour
