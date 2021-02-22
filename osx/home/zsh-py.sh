f2734d656672c7302990369daa1651e1dbe2f85c
Val Neekman <val@neekware.com>
2021-02-21 23:39:56 -0500

    upgrade to python 3.9

--- osx/home/zsh-py.sh
+++ osx/home/zsh-py.sh
@@ -7,7 +7,7 @@ export PATH="/usr/local/opt/python/libexec/bin:/usr/local/sbin:$PATH"
 PY2=/usr/local/bin/python2
 PY2DIST=/usr/local/lib/python2.7/site-packages
 PY3=/usr/local/bin/python3
-PY3DIST=/usr/local/lib/python3.7/site-packages
+PY3DIST=/usr/local/lib/python3.9/site-packages
 
 export PIP_REQUIRE_VIRTUALENV=true
 export VHOME_DIR=$HOME/Projects
