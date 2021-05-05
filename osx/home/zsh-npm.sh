f301fc6abdd5f57b7e9d367a99d8d230df544ee1
Val Neekman <val@neekware.com>
2021-05-05 11:05:59 -0400

    encapsolate nvm

--- osx/home/zsh-npm.sh
+++ osx/home/zsh-npm.sh
@@ -3,6 +3,10 @@ export NPM_TOKEN="add-your-npmjs-token-here"
 # You like yarn over npm
 export PATH="$HOME/.yarn/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+function load_nvm {
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
+load_nvm
