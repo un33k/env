3a659f6a35c5e831c50cb60758b2ac65af282de7
Val Neekman <val@neekware.com>
2021-07-29 09:47:59 -0400

    remove py alias

--- osx/home/zsh-aliases.sh
+++ osx/home/zsh-aliases.sh
@@ -1,7 +1,5 @@
 alias ls="ls -GFh"
 
-alias python="${BREW_DIR}/python3"
-
 alias djdev="export DEPLOYMENT_FLAVOR='DEVELOPMENT';"
 alias djsta="export DEPLOYMENT_FLAVOR='STAGING';"
 alias djpro="export DEPLOYMENT_FLAVOR='PRODUCTION';"
