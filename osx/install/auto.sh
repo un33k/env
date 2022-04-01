78c146f93c43998fff8ce2ac5ea49c9fdd8229bf
Val Neekman <val@neekware.com>
2022-03-31 22:27:45 -0400

    function based

--- osx/install/auto.sh
+++ osx/install/auto.sh
@@ -1,144 +1,199 @@
-#!/usr/bin/env zsh
-
+# Brew Directory arm64 vs. x86
+#######################################
 BREW_DIR="/opt/homebrew"
+# BREW_DIR="/usr/local"  # x86
 
-if [ "$1" == "intel" ]; then
-  BREW_DIR="/usr/local"
-elif [ "$1" == "apple" ]; then
-  BREW_DIR="/opt/homebrew"
-else
-  echo "usage: bash setup.sh <apple | intel>"
-  exit 1
-fi
-
-echo "Arch is set to ($1), brew directory is set to (${BREW_DIR})."
-
-# Github
+# Github Related Links
 #######################################
 GITHUB="https://raw.github.com"
 
-# Brew & Friends
+# Brew Related Links
 #######################################
-HOME_BREW="${GITHUB}/Homebrew/install/master/install.sh"
-curl -fsSL ${HOME_BREW} | bash
-brew doctor                          # Brew Doctor
-brew update                          # Brew Update
-brew install wget                    # Wget Utils
+HOME_BREW="https://raw.github.com/Homebrew/install/master/install.sh"
+
+# Iterm2 Related Links
+#######################################
+ITERM_THEME_MT="https://raw.github.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors"
+
+# Zsh Related Links
+#######################################
+OH_MY_ZSH="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+
+# Git Related Links
+#######################################
+GIT_COMPLETION="https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh"
+
+# Node Related Links
+#######################################
+NVM="https://raw.github.com/nvm-sh/nvm/v0.39.1/install.sh"
 
 # Required Paths
 #######################################
-BIN_DIR="${HOME}/.bin"               # Private Bin
-ZSH_DIR="${HOME}/.zsh"               # Directory for zsh scripts
-UTL_DIR="${HOME}/.utils"             # Directory for utility scripts
-CFG_DIR="${HOME}/.config"            # Directory for config files
-VIR_ENV="${HOME}/Sandbox/.venv"      # Virtual env directory
+BIN_DIR="${HOME}/.bin"                      # Private Bin
+ZSH_DIR="${HOME}/.zsh"                      # Directory for zsh scripts
+UTL_DIR="${HOME}/.utils"                    # Directory for utility scripts
+CFG_DIR="${HOME}/.config"                   # Directory for config files
+VIR_ENV="${HOME}/Sandbox/.venv"             # Virtual env directory
 
 # Required Directories
 #######################################
-mkdir -p ${BIN_DIR}
-mkdir -p ${ZSH_DIR}
-mkdir -p ${UTL_DIR}
-mkdir -p ${CFG_DIR}
-mkdir -p ${VIR_ENV}
+create_dirs() {
+    mkdir -p ${BIN_DIR}                     # bin directory for personal executables and scripts
+    mkdir -p ${ZSH_DIR}                     # zsh related directory
+    mkdir -p ${UTL_DIR}                     # utility directory
+    mkdir -p ${CFG_DIR}                     # config directory
+    mkdir -p ${VIR_ENV}                     # virtual env directory
+}
 
 # Required Scripts & Configurations
 #######################################
-cp ../bin/* ${BIN_DIR}
-cp ../git/.gitconfig ${HOME}
-cp ../home/.zshrc ${HOME}
-cp ../home/.npmrc ${HOME}
-cp ../home/zsh-* ${ZSH_DIR}
-cp ../py/venv/.pypirc ${HOME}
+hydrate_home_dir() {
+    echo "Hydrating home directory."
+    cp ../bin/* ${BIN_DIR}
+    cp ../git/.gitconfig ${HOME}
+    cp ../home/.npmrc ${HOME}
+    cp ../home/zsh-* ${ZSH_DIR}
+    cp ../py/.pypirc ${HOME}
+}
 
-# Iterm & Friends
+# Brew & Friends
 #######################################
-brew install iterm2                  # Terminal (Enhanced)
-ITERM_THEME_MT="${GITHUB}/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors"
-wget -q ${ITERM_THEME_MT} -O ${CFG_DIR}/material-design-colors.itermcolors
+install_brew() {
+    echo "Installing brew."
+    curl -fsSL ${HOME_BREW} | bash
+    brew doctor                             # Brew Doctor
+    brew update                             # Brew Update
+    brew install wget                       # Wget Utils
+}
 
-# Zsh & Friends
+# Build
 #######################################
-OH_MY_ZSH="${GITHUB}/ohmyzsh/ohmyzsh/master/tools/install.sh"
-curl -fsSL ${OH_MY_ZSH} | bash
-
-# Core
-#######################################
-brew install make                    # Make - Compile
-brew install travis                  # Continous Integration (CI)
+install_build() {
+    echo "Installing build tools."
+    brew install make                       # Make - Compile
+}
 
 # Extra
 #######################################
-brew install visual-studio-code      # Visual Studio Code (IDE)
-brew install google-chrome           # Google Chrome (Browser)
-brew install brave-browser           # Brave (Browser)
-brew install firefox                 # Firefox (Browser)
-brew install opera                   # Opera (Browser)
-brew install docker                  # Container Platform
-brew install slack                   # Collaboration Platform
-brew install postman                 # API Development Tool
-brew install rectangle               # Move & Resize & Grap Windows on MacOS
-brew install keybase                 # Messages & Chats Encryption
-brew install spotify                 # Music Platform
-brew install vlc                     # Media Player
-brew install alfred                  # Alfred Productivity
-brew install caffeine                # Prevents Mac Sleeping
+install_extra_apps() {
+    echo "Installing apps."
+    brew install visual-studio-code         # Visual Studio Code (IDE)
+    brew install google-chrome              # Google Chrome (Browser)
+    brew install brave-browser              # Brave (Browser)
+    brew install firefox                    # Firefox (Browser)
+    brew install docker                     # Container Platform
+    brew install postman                    # API Development Tool
+    brew install vlc                        # Media Player
+    brew install alfred                     # Alfred Productivity
+    brew install caffeine                   # Prevents Mac Sleeping
+}
 
+# Iterm & Friends
+#######################################
+install_iterm() {
+    echo "Installing iterm."
+    brew install iterm2                     # Terminal (Enhanced)
+    wget -q ${ITERM_THEME_MT} -O ${CFG_DIR}/material-design-colors.itermcolors
+}
+
+# Zsh & Friends
+#######################################
+install_zsh() {
+    echo "Installing zShell."
+    curl -fsSL ${OH_MY_ZSH} | bash          # Oh My zShell
+}
 
 # Git & Friends
 #######################################
-brew install git                     # Git Distributed Source Control
-brew install git-gui                 # Git Gui - Gitk
-GIT_COMPLETION="${GITHUB}/git/git/master/contrib/completion/git-completion.zsh"
-wget -q ${GIT_COMPLETION} -O ${ZSH_DIR}/git-completion.zsh
-
-# MemCache & Friends
-#######################################
-brew install libmemcached
+install_git() {
+    echo "Installing git."
+    brew install git                        # Git Distributed Source Control
+    brew install git-gui                    # Git Gui - Gitk
+    wget -q ${GIT_COMPLETION} -O ${ZSH_DIR}/git-completion.zsh
+}
 
 # NodeJs & Friends
 #######################################
-brew install node                    # Node JS Runtime Env
-brew install yarn                    # Node Package Manager
-NVM="${GITHUB}/nvm-sh/nvm/v0.38.0/install.sh"
-curl -fsSL ${NVM} | bash
+install_node() {
+    echo "Installing node."
+    brew install node                       # Node JS Runtime Env
+    brew install yarn                       # Node Package Manager
+    curl -fsSL ${NVM} | bash
+}
 
 # Python & Friends
 #######################################
-export PIP_REQUIRE_VIRTUALENV=''
-brew install python3                 # Python 3+
-pip3 install virtualenv               # Virtual Env
-pip3 install virtualenvwrapper        # Virtual Env Wrapper
-pip3 install pylibmc                  # Python Client for Memcache
+install_python() {
+    echo "Installing python."
+    export PIP_REQUIRE_VIRTUALENV=''
+    brew install python3                    # Python 3+
+    pip3 install virtualenv                 # Virtual Env
+    pip3 install virtualenvwrapper          # Virtual Env Wrapper
+    cp ../py/venv/postactivate ${VIR_ENV}   # Enable venv activate hook
+    export PIP_REQUIRE_VIRTUALENV=true
+}
 
 # ImageLibs & Friends (required for pillow)
 #######################################
-brew install libjpeg
-brew install libtiff
-brew install libpng
+install_image_libs() {
+    echo "Installing libs."
+    brew install libjpeg
+    brew install libtiff
+    brew install libpng
+}
+
+# MemCache & Friends
+#######################################
+install_libmemcached() {
+    echo "Installing memcached."
+    brew install libmemcached
+}
 
 # Postgres & Friends
 #######################################
-brew install postgres                # Postgresql (Relational Database)
-brew install postgis                 # Postgresql GIS extension
-brew install postico                 # Postgresql Mac Client
-initdb ${BREW_DIR}/var/postgres -E utf8
-ln -sfv ${BREW_DIR}/opt/postgres/*.plist ~/Library/LaunchAgents
-brew services start postgresql
+install_postgress() {
+    echo "Installing postgres."
+    brew install postgres                   # Postgresql (Relational Database)
+    brew install postgis                    # Postgresql GIS extension
+    brew install postico                    # Postgresql Mac Client
+    initdb ${BREW_DIR}/var/postgres -E utf8
+    ln -sfv ${BREW_DIR}/opt/postgres/*.plist ~/Library/LaunchAgents
+    brew services start postgresql
+}
 
 # Restore scripts
 # .zshrc runs after each terminal opens
 # .zprofile runs on each user login once
 # .zshrc is rewitten by this scripts & ohmyzsh
 #######################################
-cat ../home/enable >> ${HOME}/.zprofile
-cat "export BREW_DIR=${BREW_DIR}" >> ${HOME}/.zprofile
-cat ../home/enable >> ${HOME}/.zshrc
-
-# Finalize Python & Virtual Env
-#######################################
-cp ../py/venv/postactivate ${VIR_ENV}
+finalize_install() {
+    echo "finalalilzing installs."
+    cat ../home/enable >> ${HOME}/.zprofile
+    echo "export BREW_DIR=${BREW_DIR}" >> ${HOME}/.zprofile
+    cat ../home/enable >> ${HOME}/.zshrc
+}
 
 # Clean ups
 #######################################
-brew cleanup
-export PIP_REQUIRE_VIRTUALENV=true
+clean_up() {
+    echo "Clean up."
+    brew cleanup
+}
+
+# Comment out lines if not applicable
+#######################################
+# create_dirs
+# hydrate_home_dir
+# install_brew
+# install_build
+# install_extra_apps
+# install_iterm
+# install_zsh
+# install_git
+# install_node
+# install_libmemcached
+# install_python
+# install_image_libs
+# install_postgress
+# finalize_install
+# clean_up
